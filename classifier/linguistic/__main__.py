86ce213d284d2924cfb141cc599ae53590961c00
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2022-05-08 16:06:24 +0200

    full eval

--- classifier/linguistic/__main__.py
+++ classifier/linguistic/__main__.py
@@ -2,7 +2,7 @@ import logging
 
 import pandas as pd
 
-from classifier import Data
+from classifier import Data, Metric
 from classifier.linguistic.model import Model
 from classifier.util import load_config, load_logger
 
@@ -29,6 +29,7 @@ class Main:
                          generate_token=True, generate_ngrams=self.config['model']['ngrams']),
         }
 
+        self.metric = Metric(self.logger)
         self.model = Model(self.config['model'])
 
     #
@@ -48,11 +49,27 @@ class Main:
             'eval': self.model.predict(self.data['eval'].data),
         }
 
+        labels: set = {'positive', 'negative'}
+
         # print results to console
         for data_label, data in prediction.items():
-            # count valid predictions, print to console
-            valid: int = sum(pd.Series(data['prediction'] == data['gold']))
-            self.logger.info(f'ACC({data_label})\t= {valid / len(data):.4f}')
+            self.metric.reset()
+
+            for cat in labels:
+                # create confusing matrix values for each category (omitting true negative)
+                tps: int = sum(pd.Series(data['prediction'] == data['gold']))
+                fns: int = sum(pd.Series(data['gold'] == cat)) - tps
+                fps: int = sum(pd.Series(data['prediction'] == cat)) - tps
+
+                self.metric.add_tp(cat, tps)
+                self.metric.add_fn(cat, fns)
+                self.metric.add_fp(cat, fps)
+
+                # add for every other class category matches to true negative
+                for nc in (labels - {cat}):
+                    self.metric.add_tn(nc, tps)
+
+            self.metric.show()
 
 
 if __name__ == "__main__":
