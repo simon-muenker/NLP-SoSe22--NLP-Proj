5f42c3e1227f682d355525d8d007615a32031414
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2022-06-27 15:47:22 +0200

    use separate bert heard for feature attention

--- classifier/hybrid/model.py
+++ classifier/hybrid/model.py
@@ -16,9 +16,15 @@ class Model(AbsModel, nn.Module):
     def __init__(self, in_size: Tuple[int], out_size: int, config: dict):
         super().__init__(in_size, out_size, config)
 
-        self.embeds = BERTHead(
+        self.bertPred = BERTHead(
             self.config["in_size"][0],
-            self.config["in_size"][1] + self.config["out_size"],
+            self.config["out_size"],
+            self.config.copy()
+        ).to(get_device())
+
+        self.bertFeat = BERTHead(
+            self.config["in_size"][0],
+            self.config["in_size"][1],
             self.config.copy()
         ).to(get_device())
 
@@ -29,7 +35,7 @@ class Model(AbsModel, nn.Module):
             self.config["out_size"]
         ).to(get_device())
 
-        logging.info(f'> Init Neural Assemble (MLP), trainable parameters: {len(self) - len(self.embeds)}')
+        logging.info(f'> Init Neural Assemble (MLP), trainable parameters: {len(self) - len(self.bertPred) - len(self.bertFeat)}')
 
     #  -------- default_config -----------
     #
@@ -43,13 +49,13 @@ class Model(AbsModel, nn.Module):
     #  -------- forward -----------
     #
     def forward(self, data: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
-        emb: torch.Tensor = self.embeds(data[0])
+        emb: torch.Tensor = self.bertFeat(data[0])
 
         return self.output(
             self.drop(
                 torch.cat([
-                    emb[:, :-self.config["out_size"]] * data[1],
-                    emb[:, -self.config["out_size"]:]
+                    self.bertFeat(data[0]) * data[1],
+                    self.bertPred(data[0])
                 ], dim=1)
             )
         )
