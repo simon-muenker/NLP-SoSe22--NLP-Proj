75190625f277b297d17207f9e54b64ebee65e546
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2022-07-01 14:01:49 +0200

    simplified ensemble

--- classifier/hybrid/model.py
+++ classifier/hybrid/model.py
@@ -15,14 +15,12 @@ class Model(ModelFrame):
     def __init__(self, in_size: Tuple[int], out_size: int, config: dict):
         super().__init__(in_size, out_size, config)
 
-        hid_size: int = 48
-
-        self.base = Base(in_size[0], hid_size, config=config['base'])
-        self.features = Features(in_size[1], hid_size, config=config['features'])
-        self.output = Perceptron(hid_size, out_size)
+        self.base = Base(in_size[0], out_size, config=config['base'])
+        self.features = Features(in_size[1], out_size, config=config['features'])
+        self.output = Perceptron(out_size * 2, out_size)
 
         logging.info(
-            f'> Init Neural Assemble (Base+Features), trainable parameters:'
+            f'> Init Neural Assemble (Base+Features), trainable parameters: '
             f'{len(self) - (len(self.base) + len(self.features))}'
         )
 
@@ -37,4 +35,8 @@ class Model(ModelFrame):
     #  -------- forward -----------
     #
     def forward(self, data: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
-        return self.output(self.base(data[0]) * self.features(data[1]))
+        return self.output(
+            torch.concat([
+                self.base(data[0]), self.features(data[1])
+            ], dim=1)
+        )
